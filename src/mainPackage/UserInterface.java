c894cbbe98c79b7cbde6670c1b7215a25b790a6d
Justanothervitor <vitorhugomoreiradelac@gmail.com>
2023-06-30 21:00:12 -0300

    Add Screen cleaner fuction and more exception treatment

--- src/mainPackage/UserInterface.java
+++ src/mainPackage/UserInterface.java
@@ -45,6 +45,14 @@ public class UserInterface {
 	{
 		
 	}
+	
+	public static void cleanScreen ()
+	{
+		
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+
+	}
 
 	public static void printBoard(ChessPiece[][] pieces)
 	{
--- src/mainPackage/principal.java
+++ src/mainPackage/principal.java
@@ -1,7 +1,9 @@
 package mainPackage;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chessProperties.ChessException;
 import chessProperties.ChessMatchProperties;
 import chessProperties.ChessPiece;
 import chessProperties.ChessPosition;
@@ -12,17 +14,28 @@ public class principal {
 	ChessMatchProperties chessMatch = new ChessMatchProperties();
 	
 	while(true){
-	UserInterface.printBoard(chessMatch.getPieces());
-	System.out.println("");
-	System.out.print("Source:");
-	ChessPosition source = UserInterface.readChessPosition(sc);
-	
-	System.out.println();
-	System.out.print("Target:");
-	ChessPosition target = UserInterface.readChessPosition(sc);
-	
-		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-	}
+		try {
+			UserInterface.printBoard(chessMatch.getPieces());
+			System.out.println("");
+			System.out.print("Source:");
+			ChessPosition source = UserInterface.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target:");
+			ChessPosition target = UserInterface.readChessPosition(sc);
+			UserInterface.cleanScreen();
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+		catch(ChessException e){
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		}
+		catch(InputMismatchException e){
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		}
+		
+		}
 	}
 
 
